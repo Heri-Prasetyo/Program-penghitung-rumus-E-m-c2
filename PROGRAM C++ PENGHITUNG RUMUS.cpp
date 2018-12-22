8aeed06960f523e73e8a3f1f2891a68843295111
Heri Prasetyo <45659342+Heri-Prasetyo@users.noreply.github.com>
2018-12-22 10:15:00 +0700

    Add files via upload

---
+++ PROGRAM C++ PENGHITUNG RUMUS.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int E,m,c;
+
+cout<<"PROGRAM PENGHITUNG RUMUS"<<endl;
+
+cout<<"Masukan Nilai Massa            = ";
+cin>>m;
+cout<<"Masukan Nilai Kecepatan Cahaya = ";
+cin>>c;
+E=m*c*c;
+cout<<"Energinya Adalah               = "<<E;
+return 0;
+
+
+}
